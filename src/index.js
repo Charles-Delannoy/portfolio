bc453bf74de850f75cd28f84189e1743c44eed00
charles delannoy <charles.delannoy@hei.fr>
2021-01-07 15:35:19 +0100

    language management

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import { hydrate, render } from "react-dom";
 import App from './components/App';
 
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
+// const rootElement = document.getElementById("root");
+// if (rootElement.hasChildNodes()) {
+//   hydrate(<App />, rootElement);
+// } else {
+//   render(<App />, rootElement);
+// }
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
