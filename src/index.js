220b4d793a1ee503e7e1fb831148a3aa82f1f476
charles delannoy <charles.delannoy@hei.fr>
2021-01-11 19:53:04 +0100

    smooth scroll ioswith polyfill

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { hydrate, render } from "react-dom";
 import App from './components/App';
+import "scroll-behavior-polyfill";
 
 
 // const rootElement = document.getElementById("root");
